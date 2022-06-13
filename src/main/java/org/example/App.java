98ece4a26d2a54030c9ea40727efd123d1ebe8d6
riccardodegasperiSA <riccardo.degasperi@itsrizzoli.it>
2022-06-13 08:58:55 +0000

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,114 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class App {
+
+    private static Thread th;
+    private static Thread cicle;
+    private static ServerSocket serverSocket = null;
+
+    private static Socket clientSocket = null;
+
+    public static ArrayList<ClientHandler> connected = new ArrayList<>();
+
+//    the number on the left is the max number of connected users
+//    public static int size = 4 - 1;
+
+//    private static BufferedReader in = null;
+//
+//    private static PrintWriter out = null;
+
+    public static void main(String[] args) {
+
+        String hostName = new String();
+        int portNumber;
+
+        if (args.length == 2) {
+            hostName = args[0];
+            portNumber = Integer.parseInt(args[1]);
+        } else {
+            hostName = "127.0.0.1";
+            portNumber = 1234;
+        }
+
+        creaServerSocket(portNumber);
+
+        System.out.println("Server Started Hostname: " + hostName + " " + Instant.now());
+
+//        while (true){
+            ciclo(portNumber, hostName);
+//            timedMessage();
+//            for (ClientHandler ch : connected.subList(0,connected.size()-1)) {
+//                ch.getOut().println(connected.get(connected.size()-1).getUserName() + " connected");
+//            }
+//        }
+        while (true);
+    }
+
+    private static void ciclo(int portNumber, String hostName) {
+
+        cicle = new Thread(
+                ()-> {
+                    connettiClient();
+
+                    connected.add(new ClientHandler(serverSocket, clientSocket));
+
+                    connected.get(connected.size() - 1).start();
+
+//                    int count = connected.size();
+                });
+        cicle.start();
+        timedMessage();
+
+    }
+
+    private static void connettiClient() {
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("accept failed " + e);
+        }
+    }
+
+    private static void creaServerSocket(int portNumber) {
+
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            System.err.println("reader failed "+ e);
+        }
+
+    }
+
+    private static void broadCast(ClientHandler c, LocalDateTime time, int cl){
+        c.getOut().println("CP " + time + ": POLL CL" + cl);
+        System.out.println("CP " + time + ": POLL CL" + cl);
+    }
+
+//    questa parte di codice funziona in debug, ma non in esecuzione normale
+    private static void timedMessage(){
+        int i = 1;
+        th = new Thread(
+                ()->{
+                    while (true){
+                        for (ClientHandler c : connected) {
+                            broadCast(c, LocalDateTime.now(), i);
+                            try {
+                                Thread.sleep(10000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+        th.start();
+    }
+
+}
---
+++ src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,204 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler extends Thread{
+
+    private ServerSocket serverSocket;
+
+    private Socket clientSocket = null;
+
+//    private BufferedReader in = null;
+
+    private PrintWriter out = null;
+
+//    private String userName;
+
+    public PrintWriter getOut() {
+        return out;
+    }
+
+//    public BufferedReader getIn() {
+//        return in;
+//    }
+
+//    public String getUserName() {
+//        return userName;
+//    }
+
+    public ClientHandler(ServerSocket serverSocket, Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        this.serverSocket = serverSocket;
+
+        creaOut();
+
+//        if (App.connected.size() > App.size) {
+//            out.println("Server full");
+//            App.connected.remove(this);
+//            return;
+//        }
+
+//        creaIn();
+
+//        out.println("Inserisci user name");
+//
+//        try {
+//            userName = in.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(userName + " logged in");
+//        out.println("Benvenuto " + userName);
+//        broadCast(App.connected, userName + " logged in");
+    }
+
+    public void run(){
+
+//        creaIn();
+
+        creaOut();
+
+//            comunica();
+
+//            System.out.println(userName + " disconnected");
+//            broadCast(App.connected, userName + " disconnected");
+//        App.connected.remove(this);
+    }
+
+//    private void comunica() {
+//
+//        String s;
+//
+////        out.println("Inserisci user name");
+////
+////        try {
+////            userName = in.readLine();
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        }
+////        System.out.println(userName + " logged in");
+//
+////        out.println("Benvenuto " + userName);
+//
+//        try {
+//
+//            while ((s = in.readLine()) != null) {
+//                if (s.equals("++exit++")) {
+//                    out.println("bye bye");
+//                    break;
+//                }
+//                System.out.println(userName + ": " + s);
+//                process(s);
+//
+//            }
+//        } catch (IOException e) {
+//            System.err.println("Connection failed " + e);
+//        }
+//        finally {
+//            App.connected.remove(this);
+//            try {
+//                clientSocket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    private void creaOut() {
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(),true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    private void creaIn() {
+//        try {
+//            in = new BufferedReader(
+//                    new InputStreamReader(clientSocket.getInputStream()));
+//        } catch (IOException e) {
+//            System.err.println("accept failed "+ e);
+//        }
+//    }
+
+//    private void process(String s) {
+//
+//        String [] processString;
+//
+////        String result;
+//
+//        if (!s.startsWith("@")){
+//            broadCast(App.connected,s);
+//        } else {
+//            processString = s.split(" ", 2);
+//            message(processString[0].split("@")[1], processString[1]);
+//        }
+//
+////        processString = s.split(" ");
+////
+////        double x, y;
+////
+////        try {
+////            x = Double.parseDouble(processString[0]);
+////            y = Double.parseDouble(processString[2]);
+////        } catch (NumberFormatException e) {
+////            return s;
+////        } catch (ArrayIndexOutOfBoundsException e) {
+////            return s;
+////        }
+////
+////
+////        switch (processString[1]) {
+////            case "+": {
+////                result = String.valueOf((x+y));
+////                break;
+////            }
+////            case "-": {
+////                result = String.valueOf((x-y));
+////                break;
+////            }
+////            case "*": {
+////                result = String.valueOf((x*y));
+////                break;
+////            }
+////            case "/": {
+////                if (y != 0)
+////                    result = String.valueOf((x/y));
+////                else
+////                    result = "Can't divide by 0";
+////                break;
+////            }
+////            default: {
+////                return s;
+////            }
+////        }
+////        return "risposta: " + result;
+////        return result;
+//    }
+//
+//    private void broadCast(ArrayList<ClientHandler> connected, String message){
+//        message = userName + ": " + message;
+//        for (ClientHandler current : connected){
+//            if (current != this) {
+//                current.getOut().println(message);
+//            }
+//        }
+//    }
+//
+//    private void message(String target, String message){
+//        message = userName + ": " + message;
+//        for (ClientHandler current : App.connected){
+//            if (current.userName.equals(target)){
+//                current.getOut().println(message);
+//                return;
+//            }
+//        }
+//    }
+
+}
